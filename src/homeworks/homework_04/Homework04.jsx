fa75837d9489f53b8881b2fc1bc818e2ba757890
mohnapne <kuvaldin2012@gmail.com>
2024-06-13 19:30:52 +0200

    homework 04

---
+++ src/homeworks/homework_04/Homework04.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Homework04() {
+  return (
+    <div>
+      homework
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <button>submit</button>
+      </form>
+    </div>
+  );
+}
